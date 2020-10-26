a204736ea73bb9041d9230d4023248213434e3a9
Shashank Gupta <shashankcm90@gmail.com>
2020-10-26 08:38:07 -0500

    Initial Commit

--- routes/trades.js
+++ routes/trades.js
@@ -1,11 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-const tradeController = require('../controllers/trades');
-
-router.post('/', tradeController.createTrade);
-router.delete('/', tradeController.updateTrade);
-router.put('/', tradeController.updateTrade);
-router.patch('/', tradeController.updateTrade);
-
 module.exports = router;
