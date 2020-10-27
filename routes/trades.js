2d31242a67053fe302cdc9a5c41e86f2fff18519
Shashank Gupta <shashankcm90@gmail.com>
2020-10-27 08:39:53 -0500

    Final

--- routes/trades.js
+++ routes/trades.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const tradeCtrlr = require('../controllers/trades');
+
+router.delete('/:id', tradeCtrlr.noOpTrade)
+
+router.put('/:id', tradeCtrlr.noOpTrade)
+
+router.patch('/:id', tradeCtrlr.noOpTrade)
+
+router.post('/', tradeCtrlr.createTrader);
+
+router.get('/:id', tradeCtrlr.getTradeById)
+
+router.get('/', tradeCtrlr.fetchAllTrades)
+
 module.exports = router;
